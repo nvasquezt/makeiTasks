7e4b8a3b428882be571e064543bcade843b0ac96
Norbey Vasquez <lic.vasquezt@gmail.com>
2022-03-14 01:01:38 -0500

    feat: userprofile view mode

--- taskswithreact/src/components/UserProfile/UserProfile.jsx
+++ taskswithreact/src/components/UserProfile/UserProfile.jsx
@@ -1,83 +1,48 @@
-import userImage from "./userIcon.png";
 import "./UserProfile.scss";
 
 function UserProfile() {
   return (
     <div className="container">
       <div className="container_userPanel">
-        <h2>
-          <a href="#">Volver</a>
-        </h2>
+        <button>Volver</button>
         <div className="container_imageContainer">
           <img
             className="container_imageContainer--userImage"
-            src={userImage}
+            src='https://cdn-icons-png.flaticon.com/512/149/149071.png'
             alt="profile"
           />
         </div>
-        <br />
-        <br />
-        <h2>
-          <a href="#">Mis datos</a>
-        </h2>
-        <br />
-        <br />
-        <h2>
-          <a href="#">Historial de negocios</a>
-        </h2>
+        <button>Mis datos</button>
+        <button href="#">Historial de negocios</button>
       </div>
       <div className="contentProfile">
         <table className="contentProfile_userProfileTable">
           <tr>
-            <td>
-              <h2>
-                <strong>Nombre de Usuario:</strong>
-              </h2>
-            </td>
+            <td><h2><strong>Nombre de Usuario:</strong></h2></td>
             <td>userTest1</td>
           </tr>
           <tr>
-            <td>
-              <h2>
-                <strong>Nombres:</strong>
-              </h2>
-            </td>
+            <td><h2><strong>Nombres:</strong></h2></td>
             <td>Darwin</td>
           </tr>
           <tr>
-            <td>
-              <h2>
-                <strong>Apellidos:</strong>
-              </h2>
-            </td>
+            <td><h2><strong>Apellidos:</strong></h2></td>
             <td>Pedraza Carvajal</td>
           </tr>
           <tr>
-            <td>
-              <h2>
-                <strong>Correo:</strong>
-              </h2>
-            </td>
+            <td><h2><strong>Correo:</strong></h2></td>
             <td>usertest1@workit.com</td>
           </tr>
           <tr>
-            <td>
-              <h2>
-                <strong>País:</strong>
-              </h2>
-            </td>
+            <td><h2><strong>País:</strong></h2></td>
             <td>España</td>
           </tr>
           <tr>
-            <td>
-              <h2>Ciudad:</h2>
-            </td>
+            <td><h2>Ciudad:</h2></td>
             <td>Málaga</td>
           </tr>
           <tr>
-            <td>
-              <h2>Dirección:</h2>
-            </td>
+            <td><h2>Dirección:</h2></td>
             <td>Avenida siempre viva # 123</td>
           </tr>
         </table>
