14d53c69cab9621151edb74871ee9f9edb51355c
Norbey Vasquez <lic.vasquezt@gmail.com>
2022-03-28 02:42:51 -0500

    fixes

--- backendtodo/app.js
+++ backendtodo/app.js
@@ -2,42 +2,43 @@
 require('dotenv').config()
 
 const express = require('express');
-const mongooose = require('mongoose');
 
 const configureExpress = require('./config/express');
+const connectDB = require('./config/database');
 const routes = require('./routes');
 
 const app = express();
 
+connectDB();
 configureExpress(app);
 routes(app);
 
-const url = process.env.MONGODB_URI;
-mongooose.connect(url);
+// const url = process.env.MONGODB_URI;
+// mongooose.connect(url);
 
-const TaskSchema = new mongooose.Schema({
-    title: String,
-    description: String,
-    completed: Boolean
-});
+// const TaskSchema = new mongooose.Schema({
+//     title: String,
+//     description: String,
+//     completed: Boolean
+// });
 
-const Task = mongooose.model('Task', TaskSchema);
+// const Task = mongooose.model('Task', TaskSchema);
 
 
-const task = new Task({
-    title: 'Comprar pan',
-    description: 'Comprar pan para el desayuno',
-    completed: false,
+// const task = new Task({
+//     title: 'Comprar pan',
+//     description: 'Comprar pan para el desayuno',
+//     completed: false,
     
-}); 
+// }); 
 
-Task.findById('62415feff77278d86a65b576', (err, task) => {
-    if(err) {
-        console.log(err);
-    } else {
-        console.log(task);
-    }
-});
+// Task.findById('62415feff77278d86a65b576', (err, task) => {
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log(task);
+//     }
+// });
 
 
 const port= process.env.PORT || 3031;
