f1bfc3efe290105d0e93f91c057abe3f1fe60050
Norbey Vasquez <lic.vasquezt@gmail.com>
2022-03-28 10:26:16 -0500

    user api

--- backendtodo/routes.js
+++ backendtodo/routes.js
@@ -4,10 +4,12 @@
 
 const helloWorld = require('./api/helloWorld');
 const tasks = require('./api/tasks');
+const user= require('./api/user');
 
 function routes(app) {
     app.use('/api/helloworld', helloWorld);
     app.use('/api/tasks', tasks);
+    app.use('/api/user', user);
 }
 
 module.exports = routes;
